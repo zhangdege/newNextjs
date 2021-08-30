189728cae0d88b275f770962bc14604ad7be4bd7
zhangdege <908481391@qq.com>
2021-08-30 18:29:57 +0800

    add news pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
+import { Box } from '@chakra-ui/react'
 import type { NextPage } from 'next'
+import React from 'react'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-	return <div className={styles.container}>首页</div>
+	return <Box className={styles.container}>首页</Box>
 }
 
 export default Home
---
+++ pages/news/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+interface indexProps {}
+
+const index: React.FC<indexProps> = () => {
+	return <div>信息页面</div>
+}
+
+export default index
